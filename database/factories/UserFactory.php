8f05526b3169b19cc746298c5151feeb6f668f8f
wanganlin <inbox@wanganlin.com>
2023-07-04 21:48:03 +0800

    更新模型工厂类

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,8 +19,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'avatar' => fake()->image(),
+            'birthday' => fake()->date(),
+            'mobile' => '15858589988',
+            'mobile_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
