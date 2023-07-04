bf7814dd93ab488ba781d8aed4c61959d4967cfd
wanganlin <inbox@wanganlin.com>
2023-07-04 19:40:24 +0800

    增加数据转换器

---
+++ app/Http/Responses/Transformer.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Responses;
+
+abstract class Transformer
+{
+    public function transformCollection($data): array
+    {
+        return array_map([$this, 'transform'], $data);
+    }
+
+    /**
+     * @return mixed
+     */
+    abstract public function transform($item);
+}
