88ad9af147f25302d5edea16ee2bc3f9cd06966c
wanganlin <inbox@wanganlin.com>
2023-07-08 21:08:12 +0800

    增加页面

--- phpmall-web/src/components/Header.vue
+++ phpmall-web/src/components/Header.vue
@@ -6,7 +6,12 @@ import { RouterLink } from 'vue-router'
 <template>
   <div>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
+      <RouterLink :to="{name: 'index'}">首页</RouterLink> | 
+      <RouterLink :to="{name: 'catalog'}">全部目录</RouterLink> | 
+      <RouterLink :to="{name: 'category'}">分类</RouterLink> | 
+      <RouterLink :to="{name: 'article'}">文章</RouterLink> | 
+      <RouterLink :to="{name: 'brand'}">品牌</RouterLink> | 
+      <RouterLink :to="{name: 'goods'}">商品</RouterLink>
     </nav>
   </div>
 </template>
--- phpmall-web/src/router/index.ts
+++ phpmall-web/src/router/index.ts
@@ -5,7 +5,12 @@ import 'nprogress/nprogress.css'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'index', component: () => import('@/pages/Index.vue') }
+    { path: '/', name: 'index', component: () => import('@/pages/Index.vue') },
+    { path: '/article', name: 'article', component: () => import('@/pages/Article.vue') },
+    { path: '/brand', name: 'brand', component: () => import('@/pages/Brand.vue') },
+    { path: '/catalog', name: 'catalog', component: () => import('@/pages/Catalog.vue') },
+    { path: '/category', name: 'category', component: () => import('@/pages/Category.vue') },
+    { path: '/goods', name: 'goods', component: () => import('@/pages/Goods.vue') }
   ]
 })
 
