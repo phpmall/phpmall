838414ea45e1b5f6a255bd3d2aa17ee8c44ecb76
wanganlin <inbox@wanganlin.com>
2023-07-08 15:28:50 +0800

    增加部署脚本

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+cur_dir=$(pwd)
+
+git pull
+
+FrontendBuild()
+{
+    local module="$1"
+    local oss="$2"
+    cd $cur_dir/phpmall-${module}
+
+    pnpm install
+
+    if [ ${module} = "mobile" ]; then
+        pnpm run build:h5
+    else
+        pnpm run build-only
+    fi
+
+    if [ ${module} = "mobile" ]; then
+        ossutil64 cp dist/build/h5/* oss://${oss}
+    else
+        ossutil64 cp dist/* oss://${oss}
+    fi
+}
+
+FrontendBuild admin phpmall-console
+FrontendBuild mobile phpmall-mobile
+FrontendBuild passport phpmall-passport
+FrontendBuild seller phpmall-seller
+FrontendBuild supplier phpmall-supplier
+FrontendBuild user phpmall-home
+FrontendBuild web phpmall-official
+
+cd $cur_dir/phpmall-server
+./scripts/docker-build.sh
