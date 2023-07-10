d43fcbfa7cab8e22f4814bbfb59675a720ff4773
wanganlin <inbox@wanganlin.com>
2023-07-10 11:46:27 +0800

    增加页面测试

--- phpmall-admin/src/pages/Index.vue
+++ phpmall-admin/src/pages/Index.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-
+import { ref } from 'vue'
+const value1 = ref('')
 </script>
 
 <template>
   <main>
-    Index
+    Index page 
+    
+    <el-button>我的一个按钮</el-button>
+    
+    <el-date-picker
+        v-model="value1"
+        type="date"
+        placeholder="Pick a day"
+      />
   </main>
 </template>
 
--- phpmall-server/public/static/common/js/common.js
+++
@@ -1,7 +0,0 @@
-function firstLetterToLowerCase(str) {
-    return str.charAt(0).toLowerCase() + str.slice(1)
-}
-
-function convertToLowerCase(str, separator = '-') {
-    return str.replace(/[A-Z]/g, (match) => separator + match.toLowerCase())
-}
