c9777e650a4cc163c3ccc0f2dd50d628ead83549
wanganlin <inbox@wanganlin.com>
2023-07-20 21:33:56 +0800

    更新phpmall-auth

--- phpmall-auth/env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module 'element-plus/dist/locale/zh-cn.mjs'
-declare module 'nprogress'
--- web/src/layouts/Portal.vue
+++ web/src/layouts/Portal.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import Header from '@/components/Header.vue'
 </script>
 
