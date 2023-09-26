7e371754e0f35056bce34b29c26d3fcdf735fb59
wanganlin <inbox@wanganlin.com>
2023-09-26 14:30:07 +0800

    更新接口文档

--- docs/api/swagger-initializer.js
+++ docs/api/swagger-initializer.js
@@ -4,12 +4,12 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
-        {name: '运营模块', url: '/swagger/admin.json'},
         {name: '认证模块', url: '/swagger/auth.json'},
-        {name: '门户模块', url: '/swagger/portal.json'},
+        {name: '运营模块', url: '/swagger/admin.json'},
         {name: '卖家模块', url: '/swagger/seller.json'},
         {name: '供应商模块', url: '/swagger/supplier.json'},
-        {name: '买家模块', url: '/swagger/user.json'}
+        {name: '买家模块', url: '/swagger/user.json'},
+        {name: '门户模块', url: '/swagger/portal.json'}
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
