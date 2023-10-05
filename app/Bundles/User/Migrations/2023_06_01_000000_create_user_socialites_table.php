06b9770bc21c341bba2d13e545a8c9bf0e1981d9
wanganlin <inbox@wanganlin.com>
2023-10-05 20:00:22 +0800

    更新数据迁移

--- app/Bundles/User/Migrations/2023_06_01_000000_create_user_socialites_table.php
+++ app/Bundles/User/Migrations/2023_06_01_000000_create_user_socialites_table.php
@@ -14,14 +14,13 @@ return new class extends Migration
         Schema::create('user_socialites', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('user_id')->comment('用户ID');
-            $table->string('type')->comment('凭证类型:email,wechat');
-            $table->string('identifier')->comment('唯一标识:如电子邮箱,openid');
-            $table->string('credentials')->comment('凭证:密码,token');
-            $table->rememberToken()->comment('会话Token');
-            $table->string('reset_token')->comment('重置Token');
+            $table->string('type')->comment('凭证类型');
+            $table->string('identifier')->unique()->comment('标识');
+            $table->string('credentials')->comment('凭证或token');
+            $table->dateTime('verified_time')->comment('验证时间');
+            $table->unsignedInteger('status')->comment('状态:1正常,2禁用');
             $table->timestamps();
             $table->softDeletes();
-            $table->unique(['identifier', 'deleted_at']);
             $table->comment('用户社会化登录表');
         });
     }
