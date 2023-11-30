f84019f142acf541f3b67e614fccfa4bf08ed7b4
wanganlin <inbox@wanganlin.com>
2023-11-30 13:10:02 +0800

    更新模块目录

---
+++ app/Api/Auth/Controllers/AuthenticatedSessionController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Api\Auth\Controllers;
+
+use App\Api\Auth\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): Response
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return response()->noContent();
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+}
---
+++ app/Api/Auth/Tests/RegistrationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Api\Auth\Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_new_users_can_register(): void
+    {
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertNoContent();
+    }
+}
