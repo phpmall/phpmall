0330e40357e7c1b5aacf48c56d114593927387fa
wanganlin <inbox@wanganlin.com>
2023-11-30 13:25:41 +0800

    更新模块加载

--- app/Providers/BundleServiceProvider.php
+++ app/Providers/BundleServiceProvider.php
@@ -9,29 +9,40 @@ use Illuminate\Support\Str;
 
 class BundleServiceProvider extends ServiceProvider
 {
-    public function register(): void
+    public function boot(): void
     {
-        $dirs = array_merge(
-            glob(base_path('app/Api/*/')),
-            glob(base_path('app/Bundles/*/'))
-        );
-
-        foreach ($dirs as $dir) {
-            $migration = $dir.'Migrations';
+        foreach ($this->getDirs() as $dir) {
+            $module = basename($dir);
+            $migration = $dir . 'Migrations';
             if (is_dir($migration)) {
                 $this->loadMigrationsFrom($migration);
             }
 
-            $route = $dir.'Routes/web.php';
+            $route = $dir . 'Routes/web.php';
             if (is_file($route)) {
                 $this->loadRoutesFrom($route);
             }
 
-            $view = $dir.'Views';
+            $view = $dir . 'Views';
             if (is_dir($view)) {
                 $provider = basename($dir);
                 $this->loadViewsFrom($view, Str::camel($provider));
             }
+
+            $asset = $dir . 'Assets';
+            if (is_dir($view)) {
+                $this->publishes([
+                    $asset => public_path('assets/' . $module),
+                ], 'public');
+            }
         }
+    }
+
+    private function getDirs(): array
+    {
+        return array_merge(
+            glob(app_path('Api/*/')),
+            glob(app_path('Bundles/*/'))
+        );
     }
 }
