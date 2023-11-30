5b56349b4143ca0b47485891c5bffdd2c196efa4
wanganlin <inbox@wanganlin.com>
2023-11-30 15:37:10 +0800

    更新数据迁移

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique()->comment('登录用户名');
-            $table->string('password')->comment('登录用户密码');
-            $table->string('password_salt')->comment('用户密码盐值');
             $table->string('name')->comment('昵称');
             $table->string('avatar')->comment('头像');
             $table->date('birthday')->comment('生日');
+            $table->string('email')->unique()->comment('登录用户邮箱');
+            $table->timestamp('email_verified_at')->nullable()->comment('邮箱验证时间');
+            $table->string('password')->comment('登录用户密码');
             $table->unsignedTinyInteger('status')->comment('状态:1正常;2禁用');
             $table->rememberToken()->comment('会话令牌');
             $table->timestamps();
@@ -27,7 +27,7 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('username')->primary();
+            $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
             $table->comment('用户密码重置表');
