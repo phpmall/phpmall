da528c46e6a7c0e93b64d43a277f491707a2daba
wanganlin <inbox@wanganlin.com>
2023-12-12 17:14:49 +0800

    更新项目工程目录

--- build_release.sh
+++ build_release.sh
@@ -13,8 +13,8 @@ fi
 
 BackendBuild()
 {
-    cd $cur_dir
-    composer u --no-dev -o
+    cd $cur_dir/phpmall-server
+    composer u --no-dev -oW
     php artisan optimize
     php artisan migrate --force
     php artisan db:seed --force
@@ -23,24 +23,24 @@ BackendBuild()
 
 FrontendBuild()
 {
-    cd $cur_dir/frontend
+    cd $cur_dir/phpmall-web
     pnpm install
     pnpm run build
-    ossutil cp -rf dist oss://shprint/ --endpoint=oss-cn-hongkong.aliyuncs.com
+    ossutil cp -rf dist oss://phpmall-demo/ --endpoint=oss-cn-shanghai.aliyuncs.com
 }
 
 MobileBuild()
 {
-    cd $cur_dir/mobile
+    cd $cur_dir/phpmall-mobile
     pnpm install
     pnpm run build:h5
-    ossutil cp -rf dist/build/h5 oss://shprint/mobile --endpoint=oss-cn-hongkong.aliyuncs.com
+    ossutil cp -rf dist/build/h5 oss://phpmall-demo/mobile --endpoint=oss-cn-shanghai.aliyuncs.com
 }
 
 DocsBuild()
 {
-    cd $cur_dir
-    ossutil cp -rf docs/api oss://shprint/api --endpoint=oss-cn-hongkong.aliyuncs.com
+    cd $cur_dir/phpmall-docs
+    ossutil cp -rf api oss://phpmall-demo/api --endpoint=oss-cn-shanghai.aliyuncs.com
 }
 
 if [[ "${Stack}" = "all" ]]; then
