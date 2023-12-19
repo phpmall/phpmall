afdbb76f5145a542df20c1c136f0b945abaf9bfb
wanganlin <inbox@wanganlin.com>
2023-12-19 16:06:48 +0800

    增加seller模块

---
+++ resources/seller/app/login/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react'
+import { Button } from 'antd'
+
+export default function Login() {
+  return (
+    <main>
+      <div>
+        login page
+
+        <Button type='primary'>我是按钮</Button>
+      </div>
+    </main>
+  )
+}
---
+++ resources/seller/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        welcome
+      </div>
+    </main>
+  )
+}
