a6368ad558f2eeb2460fcf69420fcf10bd6bb75e
wanganlin <inbox@wanganlin.com>
2024-01-16 15:25:25 +0800

    更新配置

--- app/Api/User/Controllers/BaseController.php
+++ app/Api/User/Controllers/BaseController.php
@@ -10,6 +10,7 @@ use OpenApi\Attributes\Contact;
 
 #[OA\Info(version: '1.0', description: '提供买家API接口', title: 'API文档', contact: new Contact('API Develop Team'))]
 #[OA\Server(url: 'http://127.0.0.1:8000/api/user/', description: '开发环境')]
+#[OA\Server(url: 'https://demo.phpmall.net/api/user/', description: '演示环境')]
 #[OA\SecurityScheme(securityScheme: 'bearerAuth', type: 'http', description: 'JWT 认证信息', name: 'Authorization', in: 'header', bearerFormat: 'JWT', scheme: 'bearer')]
 abstract class BaseController extends Controller
 {
