37e6e083777e1800e416b6f0dad93bcf3beaa5fd
wanganlin <inbox@wanganlin.com>
2024-01-22 22:33:56 +0800

    更新接口文档

--- app/Api/Auth/Controllers/BaseController.php
+++ app/Api/Auth/Controllers/BaseController.php
@@ -9,8 +9,7 @@ use OpenApi\Attributes as OA;
 use OpenApi\Attributes\Contact;
 
 #[OA\Info(version: '1.0', description: '认证平台接口', title: 'API文档', contact: new Contact('API Develop Team'))]
-#[OA\Server(url: 'http://127.0.0.1:8000/api/auth/', description: '开发环境')]
-#[OA\Server(url: 'https://demo.phpmall.net/api/auth/', description: '演示环境')]
+#[OA\Server(url: '/api/auth/', description: '开发环境')]
 abstract class BaseController extends Controller
 {
 }
