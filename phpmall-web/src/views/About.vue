7acd581902c112bb3afea93cbf799ece9a90eeb6
wanganlin <inbox@wanganlin.com>
2024-01-25 15:23:36 +0800

    增加UI库

--- phpmall-web/src/views/About.vue
+++ phpmall-web/src/views/About.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const value1 = ref('')
+const size = ref<'default' | 'large' | 'small'>('default')
 </script>
 
 <template>
-    <div class="page">
-        about page content
-    </div>    
+  <div class="page">
+    about page content
+
+    <el-date-picker v-model="value1" type="date" placeholder="Pick a day" :size="size" />
+  </div>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
--- phpmall-web/src/views/Index.vue
+++ phpmall-web/src/views/Index.vue
@@ -6,5 +6,6 @@
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <el-button type="primary">Primary</el-button>
   </main>
 </template>
