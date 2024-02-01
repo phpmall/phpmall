7f8b87bccae1be00a5b937b54ae1f0bccf96a43a
wanganlin <inbox@wanganlin.com>
2024-02-01 09:55:00 +0800

    更新数据迁移

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,16 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name')->comment('昵称');
+            $table->string('avatar')->comment('头像');
+            $table->date('birthday')->comment('生日');
+            $table->string('mobile')->unique()->comment('登录手机号');
+            $table->timestamp('mobile_verified_at')->nullable()->comment('手机号验证时间');
+            $table->string('password')->comment('登录用户密码');
+            $table->unsignedTinyInteger('status')->comment('状态:1正常;2禁用');
+            $table->rememberToken()->comment('会话令牌');
             $table->timestamps();
+            $table->softDeletes();
+            $table->comment('用户表');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('mobile')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
