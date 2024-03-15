d249ff7c46e3f5cc4bd7c6ae1495f62d993b59c4
wanganlin <inbox@wanganlin.com>
2024-03-15 15:58:28 +0800

    更新模块

---
+++ app/Bundles/Wechat/Services/WechatBundleService.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bundles\Wechat\Services;
+
+use EasyWeChat\Kernel\Exceptions\InvalidArgumentException;
+use EasyWeChat\OfficialAccount\Application;
+use Illuminate\Support\Facades\Cache;
+
+class WechatBundleService
+{
+    /**
+     * 获取微信公众平台实例
+     *
+     * @throws InvalidArgumentException
+     */
+    public function officialAccount(): Application
+    {
+        $config = config('wechat.official_account');
+
+        $app = new Application($config);
+        $app->setCache(Cache::store());
+
+        return $app;
+    }
+}
--- modules/CMS/Migrations/Content/Services/FormBundleService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Bundles\CMS\Migrations\Content\Services;
-
-class FormBundleService
-{
-}
